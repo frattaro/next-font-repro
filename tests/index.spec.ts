54f0ff1367c6ecb033ca9b08925e22bdb451ec46
Anton Frattaroli <anton@certedrive.com>
2022-12-29 14:52:14 -0500

    playwright not necessary

--- tests/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "playwright-test-coverage";
-
-test("site loads", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  const pageTitle = await page.innerText("body");
-  expect(pageTitle).toBe("Test Text");
-});
